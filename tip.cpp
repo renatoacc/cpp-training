a7ee6ac1f2285ea949b67d534a9c9504246cb542
Renato Costa <renatoaccosta@gmail.com>
2022-06-08 18:19:12 +0100

    first commit

---
+++ tip.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+
+    //tip program, first input
+
+    int tip = 0;
+
+    std::cout << "Enter tip amount: ";
+    std::cin >> tip;
+    std::cout << "You paid " << tip << " dollars.\n";
+}
